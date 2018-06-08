9cfac941eb4a791ca9ea1ba97d6678d81aa276b4
cewei Dong <bjutdcw@163.com>
2018-06-08 14:13:58 +0800

    banner modified

--- src/page/common/banner/index.js
+++ src/page/common/banner/index.js
@@ -1,151 +1,123 @@
 /*
-* @Author: cewei
-* @Date:   2018-02-09 16:45:02
-* @Last Modified by:   cewei
-* @Last Modified time: 2018-05-08 12:26:04
-*/
+ * @Author: cewei
+ * @Date:   2018-02-09 16:45:02
+ * @Last Modified by:   cewei
+ * @Last Modified time: 2018-06-05 20:34:50
+ */
 require("./index.css");
 
 
-var Slide = function(params){
-		this.autoPlay = params.autoPlay;//是否自动轮播，true代表自动轮播，false代表手动
-		this.delay    = params.delay;//自动轮播的时间间隔，单位是秒
-	};
+function Slider() {
 
-var toDo = {
-	init : function(){
-		this.load();
-		if(this.autoPlay){
-			this.slide();
-		}
-	},
-	load : function(){
-		this.timer = null;
-		this.lis  = document.querySelectorAll(".container li");
-		this.oUl  = document.querySelector(".container");
-		this.currentImg = 0;
-		this.lis.forEach(function(cur){
-			cur.style.display = 'none';
-		});
-		this.lis[this.currentImg].style.display = 'block';
-		this.renderDots();
-		this.dots = document.querySelectorAll(".container .dot");
-		this.bindEvent();
-	},
-	//绑定事件
-	bindEvent : function(){
-		var imgNum = this.lis.length,
-			_this = this,
-			current = this.currentImg;
-		//给前一张按钮绑定事件
-		var pre = document.querySelector(".container .arrow.pre");
-		pre.addEventListener('click',slidePre);
-		//给后一张按钮绑定事件
-		var next = document.querySelector(".container .arrow.next");
-		next.addEventListener('click',slideNext);
+}
 
-		//向前
-		function slidePre(){
-			if(current === 0){
-				_this.lis[current].style.display ="none";
-				_this.lis[imgNum-1].style.display ="block";
-				current = imgNum-1;
-			}else{
-				_this.lis[current].style.display ="none";
-				_this.lis[current-1].style.display ="block";
-				current--;
-			}
-			_this.dots.forEach(function(cur){
-				cur.className = "dot";
-			});
-			_this.dots[current].className = 'dot active';
-		}
-		//向后
-		function slideNext(){
-			if(current === imgNum-1){
-				_this.lis[current].style.display ="none";
-				_this.lis[0].style.display ="block";
-				current = 0;
-			}else{
-				_this.lis[current].style.display ="none";
-				_this.lis[current + 1].style.display ="block";
-				current++;
-			}
+const prototype = {
+    config: {
+        index: 0,
+        state: false,
+        pics: document.querySelector('#pics'),
+        dots: document.querySelector('#dots'),
+        pre: document.querySelector('.pre'),
+        next: document.querySelector('.next'),
+        dotsNum: document.querySelectorAll('#pics .img').length - 2,
+        timer: null
+    },
+    init: function() {
+        this.dotsInit()
+        this.bindEvent()
+    },
+    dotsInit: function() {
+        const fragment = document.createDocumentFragment()
+        let dot
+        for (let i = 0; i < this.config.dotsNum; i++) {
+            dot = document.createElement('span')
+            dot.className = 'dot'
+            dot.setAttribute('index', i)
+            if (i === 0) {
+                dot.className = 'dot active'
+            }
+            fragment.appendChild(dot)
+        }
+        this.config.dots.appendChild(fragment)
+    },
+    bindEvent: function() {
+        const _this = this
+        let index = this.config.index
+        this.config.next.addEventListener('click', function() {
+            if (_this.config.state) {
+                return
+            }
+            _this.animate(-830)
+            if (index === 4) {
+                index = 0
+            } else {
+                index++
+            }
+            _this.addColorForDots(index)
+        })
 
-			_this.dots.forEach(function(cur){
-				cur.className = "dot";
-			});
-			_this.dots[current].className = 'dot active';
-		}
+        this.config.pre.addEventListener('click', function() {
+            if (_this.config.state) {
+                return
+            }
+            _this.animate(830)
+            if (index === 0) {
+                index = 4
+            } else {
+                index--
+            }
+            _this.addColorForDots(index)
+        })
+        this.config.dots.addEventListener('click', function(e) {
+            if (/dot/.test(e.target.className)) {
+                let dotIndex = parseInt(e.target.getAttribute('index'))
+                _this.animate((dotIndex - index) * (-830))
+                index = dotIndex
+                _this.addColorForDots(index)
+            }
+        })
+    },
+    animate: function(offset) {
+        this.config.state = true
+        const time = 300
+        const interval = 10
+        const speed = offset / (time / interval)
+        let pic = this.config.pics
+        let newLeft = parseInt(pic.style.left) + offset
+        let _this = this
 
-		//给圆点绑定事件
-		document.addEventListener("click",function(e){
-			var target = e.target;
 
-			if(target.matches(".dot")){
-				//切换图片
-				_this.currentImg = [..._this.dots].indexOf(target);
-				_this.lis.forEach(function(cur){
-					cur.style.display = 'none';
-				})
-				_this.lis[_this.currentImg].style.display = "block";
+        function slide() {
+            if ((speed < 0 && newLeft < parseInt(pic.style.left)) || (speed > 0 && newLeft > parseInt(pic.style.left))) {
+                pic.style.left = parseInt(pic.style.left) + speed + 'px'
+                setTimeout(slide, interval)
+            } else {
+                _this.config.state = false
+                pic.style.left = newLeft + 'px'
+                if (parseInt(pic.style.left) < -4150) {
+                    pic.style.left = -830 + 'px'
+                }
+                if (parseInt(pic.style.left) > -600) {
+                    pic.style.left = -4150 + 'px'
+                }
+            }
+        }
+        slide()
+    },
+    addColorForDots: function(index) {
+        let dotsNum = this.config.dotsNum
+        let dots = this.config.dots.querySelectorAll('.dot')
+        for (let i = 0; i < dotsNum; i++) {
+            if (/active/.test(dots[i].className)) {
+                dots[i].className = 'dot'
+                break
+            }
+        }
+        dots[index].className = 'dot active'
+    },
+}
 
-				//切换点焦点
-				_this.dots.forEach(function(cur){
-					cur.className = "dot";
-				});
-				target.className = 'dot active';
-			}
-		})
-	},
-	//渲染需要的圆点
-	renderDots : function (){
-		var dot,
-			len     = this.lis.length,
-			dotsDiv = document.createElement("div");
-		dotsDiv.className = "dotsDiv";
-		for(let i=0;i<len;i++){
-			dot = document.createElement("div");
-			if(i === this.currentImg){
-				dot.className = 'dot active';
-			}else{
-				dot.className = 'dot';
-			}
-			dotsDiv.appendChild(dot);
-		}
-		this.oUl.appendChild(dotsDiv);
-	},
-	slide : function(){
-		var _this   = this,
-			imgNum  = this.lis.length,
-			current = this.currentImg;
+Slider.prototype = prototype
 
-		//当鼠标在banner图上的时候，停止自动轮播
-		this.oUl.addEventListener("mouseover",function(e){
-			clearInterval(_this.timer);
-		});
-		//当鼠标离开banner图，自动轮播
-		this.oUl.addEventListener("mouseout",function(e){
-			_this.timer = setInterval(function(){
-			if(current === imgNum-1){
-				_this.lis[current].style.display ="none";
-				_this.lis[0].style.display ="block";
-				current = 0;
-			}else{
-				_this.lis[current].style.display ="none";
-				_this.lis[current + 1].style.display ="block";
-				current++;
-			}
-			_this.dots.forEach(function(cur){
-				cur.className = "dot";
-			});
-			_this.dots[current].className = 'dot active';
-		},_this.delay*1000);
-		})
-	}
-
-};
-
-Slide.prototype = toDo;
-
-module.exports=Slide;
+const slider = new Slider()
+module.exports = Slider
--- src/page/index/index.js
+++ src/page/index/index.js
@@ -1,18 +1,15 @@
 /*
-* @Author: cewei
-* @Date:   2018-02-09 16:33:52
-* @Last Modified by:   cewei
-* @Last Modified time: 2018-05-08 13:00:09
-*/
+ * @Author: cewei
+ * @Date:   2018-02-09 16:33:52
+ * @Last Modified by:   cewei
+ * @Last Modified time: 2018-06-05 20:22:26
+ */
 require('./index.css');
 require('page/common/nav/index.js');
 require('page/common/header/index.js');
-var banner  = require("page/common/banner/index.js");
+var banner = require("page/common/banner/index.js");
 
-(function(){
-	var slide = new banner({
-		autoPlay : true,
-		delay    : 3
-	});
+(function() {
+	var slide = new banner();
 	slide.init()
 })();
